f4023ac3e063da24f46cbce22bc216afcea19336
Anastasiia Volianska <anastasiiavolianska@A-Volia.local>
2025-10-12 23:49:47 +0300

    Added jsBasics folder with HW files

---
+++ jsBasics/task4.js
@@ -0,0 +1,27 @@
+// Task_1
+let circleRadius = 7;
+const circleArea = Math.PI * Math.pow(circleRadius,2); 
+// альтернативний варіант 
+// const circleArea = Math.PI * circleRadius ** 2; 
+
+console.log((circleArea).toFixed(2));
+
+
+// Task_2
+let lengthRectangle = 5.3;
+let widthRectangle = 7;
+const rectangleArea = (lengthRectangle * widthRectangle);
+
+console.log((rectangleArea).toFixed(2));
+
+// альтернативний варіант 
+// roundedRectangleArea = Number(rectangleArea.toFixed(2));
+// console.log(roundedRectangleArea);
+
+
+// Task_3
+let cylinderRadius = 6;
+let cylinderHeight = 9;
+const cylinderArea = Math.PI * cylinderRadius ** 2 * cylinderHeight
+
+console.log((cylinderArea).toFixed(2));
