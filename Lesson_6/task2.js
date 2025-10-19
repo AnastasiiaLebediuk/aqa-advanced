ec60394e09854fa26d3f02572b2b038edc5ce0bb
Anastasiia Volianska <anastasiiavolianska@A-Volia.local>
2025-10-20 01:29:36 +0300

    Add folder Lesson_6

---
+++ Lesson_6/task2.js
@@ -0,0 +1,17 @@
+function ageValue (age){
+    if (age >= 18){
+        return true;
+    } else{
+        return false;
+    }
+}
+console.log(ageValue(15));
+console.log(ageValue(25));
+
+// альтернативний варіант
+
+// function ageValue (age){
+//     return age >=18;
+// }
+// console.log(ageValue(15));
+// console.log(ageValue(25));
